6029269017623b3a638d9f5fcaa7af63b2c2bcd1
Nina Filippova <nina_af@login1.frontera.tacc.utexas.edu>
2025-02-20 14:22:28 -0600

    Added option to overwrite disk snapdir if snapshots moved to different directory.

--- analyze_snapshot.py
+++ analyze_snapshot.py
@@ -101,7 +101,8 @@ class Disk:
         tracking disk particles if particle IDs are not unique.
     """
 
-    def __init__(self, fname, cloud, USE_IDX=False, overwrite_snapdir=False):
+    def __init__(self, fname, cloud, USE_IDX=False, 
+                 overwrite_snapdir=False, snapdir=None):
         
         self.fname   = fname
         self.cloud   = cloud
@@ -114,7 +115,8 @@ class Disk:
             self.snapdir      = header.attrs['snapdir']        # Snapshot data directory.
             # Use if snapshots have been moved to a different directory than stored in Header.
             if overwrite_snapdir:
-                self.snapdir  = self.fname.split('snapshot_')[0]
+                #self.snapdir  = self.fname.split('snapshot_')[0]
+                self.snapdir  = snapdir
             self.disk_type    = header.attrs['disk_type']
             self.disk_name    = header.attrs['disk_name']
             self.primary_sink = header.attrs['primary_sink']
